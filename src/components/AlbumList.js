3cc2d0896e049db8d70b0a9d7a6f2c30614ef176
tsherdiwala <tsherdiwala@gmail.com>
2017-11-02 18:45:10 +0530

    complete basic project

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
 
     componentDidMount() {
-         // eslint-disable-next-line
+        // eslint-disable-next-line
         fetch('https://rallycoding.herokuapp.com/api/music_albums')
             .then((response) => response.json())
             .then((responseData) => this.setState({ albums: responseData }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
+        console.log(this.state.albums);
         return (
-            <View>
-                <Text>Album List!!!</Text>
-            </View>
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
         );
     }
 }
